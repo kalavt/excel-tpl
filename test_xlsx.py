14beb78aed7ecab02822a0247ba9e94b1e6d3bb0
kalavt <443311+kalavt@users.noreply.github.com>
2022-03-23 15:29:38 +0800

    init

---
+++ test_xlsx.py
@@ -0,0 +1,17 @@
+import datetime
+
+import jinja2
+
+from exceltpl.xlsxDocument import xlsDocument, xlsxDocument
+
+jinja_env = jinja2.Environment()
+
+jinja_env.globals["now"] = datetime.datetime.now
+jinja_env.add_extension("jinja2.ext.do")
+
+
+data = {'product':'exceltpl',"version":'0.0.1','terminology':[{'key':'N/A','value':'Not applicable'}]}
+
+doc = xlsxDocument("examples/example.xlsx", data, jinja_env)
+doc.render()
+doc.save("examples/result.xlsx")
